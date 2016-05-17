57d7a873a039db2d02b5f9f9626b814a6aa58d8a
Guilherme Silveira da Silva <guilherme.silveira@Gustavos-Mini.avanti>
2016-05-17 11:38:55 -0300

    Alteracoes visuais e correcoes no header.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,14 @@
 angular.module('app.controllers', [])
   
 .controller('homeCtrl', function($scope) {
-
+	$scope.logo = "<img class='logo-header' src='img/logo.png'>";
 })
    
 .controller('horariosCtrl', function($scope) {
-
+	$scope.logo = "<img class='logo-header' src='img/logo.png'>";
 })
       
 .controller('horarioCtrl', function($scope) {
-
+	$scope.logo = "<img class='logo-header' src='img/logo.png'>";
 })
  
